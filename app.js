03bf17c2926479368e09156e74ac8aed16dd9a62
Gustavo Maioli <gumaioli99@gmail.com>
2023-05-24 17:00:18 -0300

    Aplicacao inicial

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const os = require("os");
+const app = express();
+
+app.get("/", (req, res) => {
+    return res.status(200).json({
+            message: "Olá"
+        })
+});
+
+app.get("/liveness", (req, res) => {
+    return res.status(200).json({
+            message: "Meu app esta vivo",
+            path: process.cwd(),
+            gid: process.getegid(),
+            uid: process.geteuid()
+        })
+});
+
+app.get("/liveness", (req, res) => {
+    return res.status(200).json({
+            message: "Meu app esta pronto",
+            plataform: os.plataform(),
+            freemem: os.freemem(),
+            homedir: os.homedir(),
+            date: new Date().getTime()
+        })
+});
+
+module.exports = app;
