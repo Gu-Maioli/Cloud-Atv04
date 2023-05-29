590d637d961ecf13ffa44a773bf8834468e59c08
Gustavo Maioli <gumaioli99@gmail.com>
2023-05-29 10:58:42 -0300

    Adicionar mysql2

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const mysql = require("mysql2");
 const os = require("os");
 const app = express();
+const cors = require("cors");
+
+//docker run -d -p 3306:3306 --name teste01 -e MYSQL_ROOT_PASSWORD=root -e MYSQL_USER=root -e MYSQL_PASSWORD=passwd -e MYSQL_DATABASE=db_aula mysql/mysql-server:latest
+
+const db = mysql.createPool({
+    host: "172.17.0.2",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "db_aula"
+});
+
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
     
@@ -11,9 +27,13 @@ app.get("/", (req, res) => {
 
 app.get("/consulta-dados", (req, res) => {
 
-    return res.status(200).json({
-            message: "teste"
-        })
+    db.query("SELECT * FROM db_aula.aluno", (err, result) => {
+        if (err)
+            return res.status(500).json(err);
+        else
+            return res.status(200).json({ message: result });
+    });
+    
 });
 
 app.get("/liveness", (req, res) => {
@@ -25,10 +45,11 @@ app.get("/liveness", (req, res) => {
         })
 });
 
-app.get("/liveness", (req, res) => {
+app.get("/readiness", (req, res) => {
+
     return res.status(200).json({
             message: "Meu app esta pronto",
-            plataform: os.plataform(),
+            plataform: os.plataform,
             freemem: os.freemem(),
             homedir: os.homedir(),
             date: new Date().getTime()
